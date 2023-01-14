f57c092d41e6efb13d355c392de342c1135d95ac
Monroe <monroedrysdale@gmail.com>
2023-01-13 20:52:05 -0800

    Add physics based rotation toward mouse
    Still needs tweaking of angular drag and turn speed values

--- Assets/Player/Scripts/Movement.cs
+++ Assets/Player/Scripts/Movement.cs
@@ -1,29 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
-using TreeEditor;
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private GameObject player;
+    private GameObject _player;
     private Rigidbody2D _rigidbodyReference;
-    public float thrust = 2.5f;
+    private float angle1;
     
+    public float thrust = 2.5f;
+    public bool hasControl = true;
+    public float turnSpeed = 2f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<GameObject>();
+        _player = GetComponent<GameObject>();
         _rigidbodyReference = GetComponent<Rigidbody2D>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Debug.Log(angle);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         
-        _rigidbodyReference.AddForce(transform.right * thrust);
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasControl)
+        {
+            faceMouse();
+            
+            _rigidbodyReference.AddForce(transform.right * thrust);
+        }
+    }
+    
+    void faceMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);    
+        
+        var angle = Mathf.Atan2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y) * 180/ Mathf.PI; //Get mouse angle and parse it
+
+        angle -= 90; //account for sprite rotated to right by default
+        
+        _rigidbodyReference.rotation %= 360; //normalize rigidbody rotation
+        
+        angle = (angle + _rigidbodyReference.rotation); // Summate rigidbody and mouse angle
+        if (angle < 0)
+        {
+            angle1 = 360.0f + angle;
+        }
+        else
+        {
+            angle1 = 360.0f - angle; // calculates negative angle
+        }
+
+        if (Mathf.Abs(angle) > Mathf.Abs(angle1) && angle < 0)
+        {
+            angle = angle1;  
+        }
+        if (Mathf.Abs(angle) > Mathf.Abs(angle1) && angle > 0)
+        {
+            angle = angle1 * -1; // from my testing (stolen) i found out that by writing these ifs rigid body stops doing awkward 360 spins
+        }
+        _rigidbodyReference.AddTorque(-angle / 180 * turnSpeed);
     }
 }
